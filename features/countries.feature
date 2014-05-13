d2e51162b9d0ac92ba58591701b1e9f7e1ebdfc8
Hajo Eichler <hajo.eichler@commercetools.de>
2014-05-13 22:24:35 +0200

    Make countries command work.

--- features/countries.feature
+++ features/countries.feature
@@ -1,6 +1,6 @@
 Feature: Country commands
 
-  Scenario: Add countries
+  Scenario: Set countries on a project
     Given I am logged in and select a new project
-    When I run `sphere country DE,AT`
+    When I run `sphere country set DE,AT`
     Then the exit status should be 0
--- lib/sphere-cli/commands/countries.rb
+++ lib/sphere-cli/commands/countries.rb
@@ -2,33 +2,34 @@ desc 'Manage countries in your project'
 long_desc %(
   This command helps you to manage countries.
 )
-command [:country, : countries] do |c|
+command [:country, :countries] do |c|
 
   c.arg_name 'countries'
-  c.desc 'Create countries'
-  c.command [:"create"] do |create|
+  c.desc 'Set countries of a project'
+  c.flag [:p,:project], :desc => 'Project key to use', :type => String
+  c.command [:set] do |create|
     create.action do |global_options,options,args|
+      raise "Please provide the countries to set!" if args.empty?
       sphere.ensureLoggedIn
-      project_key = get_project_key args, false
+      project_key = get_project_key options
       res = sphere.get projects_list_url
-      #GET  VERSION
       performJSONOutput global_options, res do |data|
         if data.empty?
-          puts 'There are no projects.'
-          return
+          puts 'There are no projects!'
+        else
+          found = false
+          data.each do |d|
+            next unless d['key'] == project_key
+            found = true
+            project_version = d['version']
+            Sphere::Projects.new.add_country project_key, project_version, args[0]
+            puts "Countries of project with key '#{project_key}' update."
+          end
+          raise "Project with key '#{project_key}' does not exist!" unless found
         end
-        data.each do |d|
-          next unless d['key'] == project_key
-          project_version = d['version']
-          Sphere::Projects.new.add_country project_key project_version args
-          puts "countries update"
-          return
-        end
-        raise "Project with key '#{project_key}' does not exist."
       end
 
     end
   end
 
-  # TODO: import tax rates via csv
 end
